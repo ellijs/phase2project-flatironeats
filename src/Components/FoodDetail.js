64cadd0d694da406da0b91dd4450b48010ecfc09
Jessica J Lee <79885174+ellijs@users.noreply.github.com>
2021-08-03 14:01:32 -0400

    Update FoodDetail.js

--- src/Components/FoodDetail.js
+++ src/Components/FoodDetail.js
@@ -23,7 +23,7 @@ function FoodDetail({ food, onImageClick, setReview }){
             <h4 className="text">Rating: {emoji.repeat(food.rating)}</h4>
             <h4>Reviews: {foodReviewToDisplay} </h4>
             </div>
-            {showReview ? <ReviewForm id={food.id} showReview={showReview} setReview={setReview}/>
+            {showReview ? <ReviewForm food={food} showReview={showReview} setReview={setReview}/>
                         : 
                     <button onClick={addReviewClick}>Add Review</button>}
         </div>
