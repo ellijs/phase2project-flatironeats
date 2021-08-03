bde8c5331fc7842b13323cae43664e6d65042287
Jessica J Lee <79885174+ellijs@users.noreply.github.com>
2021-08-03 18:59:45 -0400

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,28 +1,57 @@
+
 function Cart({ myCart, removeFoodFromCart, purchaseFood }){
     function onRemoveFood(cartId){
         removeFoodFromCart(cartId)
     }
+   
     let subtotalPrice = myCart.reduce((a, b) => (a + b.price),0)
     let totalPrice = parseFloat(subtotalPrice*1.08875).toFixed(2)
 
     
     return(
-        <div>
-            <h2>My Cart</h2>
-            {myCart.map((food) => {
-                return(
-                    <div key={food.cartId}>
-                        <li>{food.name} - ${food.price}
-                            <button onClick={(e) => onRemoveFood(food.cartId)}>
-                                Remove</button>
-                        </li>
-                    </div>)                              
-                })}
-                <h4>Subtotal: ${subtotalPrice}</h4>
-                <h3>Total (inc. tax): ${totalPrice}</h3>
-            <button onClick={purchaseFood}>Purchase (just clears cart for now)</button>
+        <div className="shopping-cart-container">
+            <h1>Shopping Cart</h1>
+            <div className="cart">
+                <div className="products">
+                    {myCart.map((food) => {
+                        return(
+    
+                            <div key={food.id} className="product">
+                                <img src={food.image} />
+                                <div className="product-info">
+                                    <h3 className="product-name">{food.name}</h3>
+                                    <h3 className="product-price">Price : $ {food.price}</h3>
+                                    <button onClick={(e) => onRemoveFood(food.cartId)} className="product-remove">
+                                    Remove Item
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+                
+                <div className="cart-total">
+                    <p>
+                        <span>
+                            Number of Items
+                        </span>
+                        <span>{myCart.length}</span>
+                    </p>
+                    <p> 
+                        <span>Subtotal</span>
+                        <span>$ {subtotalPrice}</span>
+                    </p>
+                    <p> 
+                        <span>Total (inc.tax)</span>
+                        <span>$ {totalPrice}</span>
+                    </p>
+                    <button onClick={purchaseFood} href="#">Proceed to checkout</button>
+                </div>
+    
+            </div>
         </div>
     )
 }
 
 export default Cart;
+
