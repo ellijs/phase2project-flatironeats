95f489497df5ab3de18f555472e19f1b11f03ab3
Jessica J Lee <79885174+ellijs@users.noreply.github.com>
2021-08-04 13:56:03 -0400

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -39,7 +39,7 @@ function App() {
     function addNewReview(value, food, length, id){
     const newReviewObj = { id: (length + 1), content: value }
     
-    fetch(`http://localhost:3000/foods/${id}/` , {
+    fetch(`http://localhost:4000/foods/${id}/` , {
       method: "PATCH",
       headers: {
         "Content-Type" : "application/json"
