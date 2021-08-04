5e29e8a80dc75d8781fba528c642404e2dd8bfda
Jessica J Lee <79885174+ellijs@users.noreply.github.com>
2021-08-04 19:05:06 -0400

    Create ItemsInCart.js

---
+++ src/Components/ItemsInCart.js
@@ -0,0 +1,46 @@
+function ItemsInCart ({ food, myCart, setMyCart, removeFoodFromCart }) {
+    function onRemoveFood(cartId){
+        removeFoodFromCart(cartId)
+    }
+
+    function handleIncreaseQuantity() {
+        setMyCart([...myCart].map((item) => {
+            if(item.id === food.id) {
+              item.quantity += 1;
+              return item
+            } else {
+              return item;
+            }}
+        ))
+    }
+
+    function handleDecreaseQuantity() {
+        setMyCart([...myCart].map((item) => {
+            if(item.id === food.id) {
+              item.quantity -= 1;
+              return item
+            } else {
+              return item;
+            }}
+        ))
+    }
+
+    return(
+      <div key={food.id} className="product">
+        <img src={food.image} alt={food.name}/>
+        <div className="product-info">
+            <h3 className="product-name">{food.name}</h3>
+            <h3 className="product-price">Price : $ {food.price}</h3>           
+            <h3 className="product-quantity">Quantity : {food.quantity}</h3>
+            <button onClick={handleIncreaseQuantity}>+</button>
+            <button onClick={handleDecreaseQuantity}>-</button>
+            <button onClick={(e) => onRemoveFood(food.cartId)}
+            className="product-remove">
+            Remove Item
+            </button>
+        </div>
+      </div>
+    )
+}
+
+export default ItemsInCart
